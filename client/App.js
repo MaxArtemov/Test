695a619ddc4b7acc59b2724e682c620ba58969ee
Max <maxartemov90@gmail.com>
2019-07-10 20:21:32 +0300

    remove func

--- client/App.js
+++ client/App.js
@@ -25,11 +25,3 @@ render(
  <App />,
   document.getElementById('app')
 )
-
-function a() {
-    console.log('1')
-    console.log('2')
-    console.log('3')
-    console.log('4')
-    console.log('5')
-}
